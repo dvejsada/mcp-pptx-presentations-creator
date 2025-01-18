c36d25ffcd22df7e82092f92bb1ccbda857731c8
Daniel Vejsada <dan.vejsada@gmail.com>
2025-01-18 14:40:17 +0100

    Add section slides, amending input schema.

--- src/create_pptx.py
+++ src/create_pptx.py
@@ -5,6 +5,8 @@ import io
 
 TEMPLATE_REGULAR = "/app/src/prk_template_4_3.pptx"
 TEMPLATE_WIDE = "/app/src/prk_template_16_9.pptx"
+# TEMPLATE_REGULAR = "prk_template_4_3.pptx"
+# TEMPLATE_WIDE = "prk_template_16_9.pptx"
 
 TITLE_SLIDE_LAYOUT = 2
 SECTION_SLIDE_LAYOUT = 7
@@ -12,42 +14,44 @@ TITLE_AND_CONTENT_LAYOUT = 4
 
 class PowerpointPresentation:
 
-    def __init__(self, title: str, author: str, slides: list, format: str):
+    def __init__(self, author: str, slides: list, format: str):
 
-        self.title = title
         self.author = author
         self.slides: list = slides
 
         if format == "4:3":
             self.presentation = Presentation(TEMPLATE_REGULAR)
         elif format == "16:9":
-            self.presentation = Presentation(TEMPLATE_REGULAR)
+            self.presentation = Presentation(TEMPLATE_WIDE)
         else:
             self.presentation = Presentation(TEMPLATE_REGULAR)
 
-        self.create_title_slide()
 
         for slide in self.slides:
-            self.create_content_slide(slide)
+            if slide["slide_type"] == 2:
+                self.create_content_slide(slide)
+            elif slide["slide_type"] == 1:
+                self.create_section_header_slide(slide)
+            elif slide["slide_type"] == 0:
+                self.create_title_slide(slide)
 
-
-    def create_title_slide(self):
+    def create_title_slide(self, slide: dict):
         title_layout = self.presentation.slide_layouts[TITLE_SLIDE_LAYOUT]
         title_slide = self.presentation.slides.add_slide(title_layout)
-        title_slide.placeholders[0].text = self.title
+        title_slide.placeholders[0].text = slide["slide_title"]
         title_slide.placeholders[1].text = self.author
 
     def create_section_header_slide(self, slide: dict):
         section_layout = self.presentation.slide_layouts[SECTION_SLIDE_LAYOUT]
         section_slide = self.presentation.slides.add_slide(section_layout)
-        section_slide.placeholders[0].text = slide["title"]
+        section_slide.placeholders[0].text = slide["slide_title"]
 
     def create_content_slide(self, slide: dict[str]):
         content_layout = self.presentation.slide_layouts[TITLE_AND_CONTENT_LAYOUT]
         content_slide = self.presentation.slides.add_slide(content_layout)
-        content_slide.placeholders[0].text = slide["title"]
+        content_slide.placeholders[0].text = slide["slide_title"]
 
-        slide_content: str = slide["content"]
+        slide_content: str = slide["slide_text"]
         paragraphs: list = slide_content.split("\n")
 
         content_slide.placeholders[1].text = ""
@@ -67,11 +71,11 @@ class PowerpointPresentation:
         file_like_object.seek(0)
         return file_like_object
 
-def create_presentation(title: str, author: str, slides: list, format: str) -> str:
+def create_presentation(author: str, slides: list, format: str) -> str:
     """Creates new presentation."""
 
     # Create presentation
-    presentation = PowerpointPresentation(title, author, slides, format)
+    presentation = PowerpointPresentation(author, slides, format)
 
 
     # Save presentation
--- src/server.py
+++ src/server.py
@@ -16,7 +16,7 @@ def create_server():
 
     init_options = InitializationOptions(
         server_name="mcp-pptxcreator",
-        server_version="0.4",
+        server_version="0.5",
         capabilities=server.get_capabilities(
             notification_options=NotificationOptions(),
             experimental_capabilities={},
@@ -37,10 +37,6 @@ def create_server():
                 inputSchema={
                     "type": "object",
                     "properties": {
-                        "title": {
-                            "type": "string",
-                            "description": "Title of the presentation."
-                        },
                         "author": {
                             "type": "string",
                             "description": "Name of the author."
@@ -48,28 +44,33 @@ def create_server():
                         "format": {
                             "type": "string",
                             "enum": ["4:3", "16:9"],
+                            "default":"4:3",
                             "description": "Format of the presentation, either 4:3 or 16:9. Will default to 4:3 if not specified."
                         },
                         "slides": {
                             "type": "array",
-                            "description": "Individual slides content. One slide per list item, do not include title slide.",
+                            "description": "Individual slides content. One slide per list item,.",
                             "items": {
                                 "type": "object",
                                 "properties": {
-                                    "title": {
+                                    "slide_type": {
+                                        "type": "integer",
+                                        "description": "Type of slide layout - use 0 for title slide, 1 for section divider or 2 for text content slide.",
+                                    },
+                                    "slide_title": {
                                         "type": "string",
                                         "description": "Title of the slide."
                                     },
-                                    "content": {
+                                    "slide_text": {
                                         "type": "string",
-                                        "description": "Text of the slide in paragraphs. Each paragraph shall be separated by newline. Text of each paragraph must be prefixed by %1, %2 for indentation level."
+                                        "description": "Text of the slide in paragraphs. Each paragraph shall be separated by newline. Text of each paragraph must be prefixed by %1 or %2 for indentation level. Do not include for title and section slide layouts."
                                     }
                                 },
-                                "required": ["title", "content"]
+                                "required": ["slide_type","title"]
                             }
                         }
                     },
-                    "required": ["title", "slides"]
+                    "required": ["slides"]
                 }
             ),
         ]
@@ -86,13 +87,13 @@ def create_server():
 
 
         if name == "create-powerpoint-presentation":
-            title: str = arguments.get("title")
+
             author: str = arguments.get("author")
             pptx_format: str = arguments.get("format")
             slides: list = arguments.get("slides")
 
-            if not title or not slides:
-                raise ValueError("Missing parameter")
+            if not slides:
+                raise ValueError("Missing slides")
 
             if not author:
                 author = "Unknown Author"
@@ -100,7 +101,7 @@ def create_server():
             if not pptx_format:
                 pptx_format = "4:3"
 
-            result_text = create_presentation(title, author,slides, pptx_format)
+            result_text = create_presentation(author,slides, pptx_format)
 
             return [
                 types.TextContent(
